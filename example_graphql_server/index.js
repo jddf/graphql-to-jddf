0ae9d9d3bca3cf7ec00ec4032ec203d67eb3a4cb
Ulysse Carion <ulysse@segment.com>
2020-01-20 13:43:14 -0800

    Clean up example_graphql_server

--- example_graphql_server/index.js
+++ example_graphql_server/index.js
@@ -11,15 +11,3 @@ app.use(
 );
 
 app.listen(4000);
-
-// var app = express();
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     schema: schema,
-//     rootValue: root,
-//     graphiql: true
-//   })
-// );
-// app.listen(4000);
-// console.log("Running a GraphQL API server at http://localhost:4000/graphql");
